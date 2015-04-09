70175bbfb5419b6d7a8bf40e307d3d925591a3e6
Emir Djenasevic <emir.dj93@gmail.com>
2015-04-09 23:17:28 +0200

    dodan padajuci meni

---
+++ js/avioagency.js
@@ -0,0 +1,28 @@
+var Item;
+var Timer;
+document.onclick = CloseMenu;
+
+
+function OpenMenu(Menu)
+{
+	if (Item) Item.style.visibility = "hidden";
+  	Item = document.getElementById(Menu);
+  	Item.style.visibility = "visible";
+}
+
+
+function CloseMenu()
+{
+	Timer = window.setTimeout(PerformClose, 10);
+}
+
+function PerformClose()
+{
+	if (Item) Item.style.visibility = "hidden";
+}
+
+
+function KeepSubmenu()
+{
+	window.clearTimeout(Timer);
+}
